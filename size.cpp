5fd655b54630b8c95897e2e7a82e8233dad6945e
parthm2310 <86843036+parthm2310@users.noreply.github.com>
2021-09-04 13:16:45 +0530

    Delete size.cpp

--- size.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-	cout<<"The size of char is : "<<sizeof(char)<<" byte\n";
-	cout<<"The size of int is : "<<sizeof(int)<<" bytes\n";
-	cout<<"The size of float is : "<<sizeof(float)<<" bytes\n";
-	cout<<"The size of double is : "<<sizeof(double)<<" bytes\n";
-}
